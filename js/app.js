f5928a6c978e363cbc38424c58c64fdb893a9edd
meilani <lauraho2006@gmail.com>
2020-05-30 17:21:58 -0400

    finished validation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 const submit = document.querySelector("#submit");
 const email = document.querySelector("#email");
 const invalidText = document.querySelector("#invalid-text");
+const validText = document.querySelector("#valid-text");
 
 function validEmail (e) {
-    if (email.validity.valid) {
-        email.style.borderColor = "#C2D3FF";
+    e.preventDefault();
+    if (email.validity.valid && email.value !== "") {
+        email.style.borderColor = "#2ECC71";
         invalidText.style.display = "none";
+        validText.style.display = "block";
+        email.value = "";
         
     } else {
         email.style.borderColor = "#ff5466";
         invalidText.style.display = "block";
+        validText.style.display = "none";
 
     }
 }
